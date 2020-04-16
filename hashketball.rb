64aa86852c2ec3018ef920f170cd5b717d573aed
Trevor Low <tlow94@uw.edu>
2020-04-16 21:00:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -183,6 +183,7 @@ end
 #* Build a method, `player_stats`, that takes in an argument of a player's name
 #  and returns a hash of that player's stats.
 def player_stats (player_name)
+  binding.pry
   if players = game_hash[:home][:players] || game_hash[:away][:players]
     return players[player_name]
 end
